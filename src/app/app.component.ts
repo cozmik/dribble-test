f7690e4a8bde2757e2eb899d6c576a4360a5de1c
Kelvin Esekhile <cozmik05@gmail.com>
2022-08-26 23:38:36 +0100

    test done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {faBagShopping} from "@fortawesome/free-solid-svg-icons/faBagShopping";
+import {faArrowRight, faMinus, faPlay, faPlus, faQuoteLeft} from "@fortawesome/free-solid-svg-icons";
+import {faPaperPlane} from "@fortawesome/free-regular-svg-icons";
+import {
+  fadeInLeftAnimation,
+  fadeInLeftOnEnterAnimation, fadeInRightAnimation,
+  fadeInRightOnEnterAnimation, fadeInUpAnimation,
+  fadeInUpOnEnterAnimation,
+  slideInDownOnEnterAnimation,
+  slideInLeftOnEnterAnimation, slideInRightAnimation,
+  slideInRightOnEnterAnimation, slideInUpAnimation,
+  slideInUpOnEnterAnimation, zoomInAnimation,
+  zoomInOnEnterAnimation
+} from "angular-animations";
+import {animate} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    slideInDownOnEnterAnimation({animateChildren: "together"}),
+    slideInLeftOnEnterAnimation({animateChildren: "together"}),
+    slideInRightAnimation({animateChildren: "together"}),
+    slideInRightOnEnterAnimation({animateChildren: "together"}),
+    slideInUpOnEnterAnimation({animateChildren: "together"}),
+    slideInUpAnimation({animateChildren: "together"}),
+    slideInUpAnimation({animateChildren: "together"}),
+    fadeInUpOnEnterAnimation({animateChildren: "together"}),
+    fadeInUpAnimation({animateChildren: "together"}),
+    zoomInOnEnterAnimation(),
+    zoomInAnimation(),
+    fadeInRightOnEnterAnimation({animateChildren: "together"}),
+    fadeInRightAnimation({animateChildren: "together"}),
+    fadeInLeftAnimation({animateChildren: "together"})
+  ]
 })
 export class AppComponent {
+  items = [
+    './assets/images/cream1.png',
+    './assets/images/cream2.png',
+    './assets/images/cream3.png',
+    './assets/images/cream4.png',
+    './assets/images/cream5.png'
+  ];
+
+  companies = [
+    './assets/images/company1.png',
+    './assets/images/company2.png',
+    './assets/images/company3.png',
+    './assets/images/company4.png',
+  ]
+
+  igPictures = [
+    './assets/images/ig2.png',
+    './assets/images/ig3.png',
+    './assets/images/ig4.png',
+    './assets/images/ig5.png',
+  ]
+
+
+  bag = faBagShopping;
+  arrowRight = faArrowRight;
+  play = faPlay;
+  minus = faMinus;
+  plus = faPlus;
+  quote = faQuoteLeft;
+  send = faPaperPlane;
+
   title = 'dribble-test';
+  animateTrending = false;
+  animateMoreDetails = false;
+  animateMoreContent = false;
+  animateFaq = false;
+  animateComments = false;
+  animateIg = false;
+  animateFooter = false;
+
+  onIntersection(event: IntersectionObserverEntry[]) {
+    console.log(event[0]);
+    if(event[0].isIntersecting) {
+      this.animateTrending = true;
+    }
+  }
+
+  animateMoreInfo(event: IntersectionObserverEntry[]) {
+    if(event[0].isIntersecting) {
+      this.animateMoreDetails = true;
+    }
+  }
+
+  animateContent2(event: IntersectionObserverEntry[]) {
+    if(event[0].isIntersecting) {
+      this.animateMoreContent = true;
+    }
+  }
+
+  animateFaqFn(event: IntersectionObserverEntry[]) {
+    if(event[0].isIntersecting) {
+      this.animateFaq = true;
+    }
+  }
+
+  animateCommentFn(event: IntersectionObserverEntry[]) {
+    if(event[0].isIntersecting) {
+      this.animateComments = true;
+    }
+  }
+
+  animateIgFn(event: IntersectionObserverEntry[]) {
+    if(event[0].isIntersecting) {
+      this.animateIg = true;
+    }
+  }
+
+  animateFooterFn(event: IntersectionObserverEntry[]) {
+    if(event[0].isIntersecting) {
+      this.animateFooter = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {IntersectionObserverModule} from "@ng-web-apis/intersection-observer";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    IntersectionObserverModule
   ],
   providers: [],
   bootstrap: [AppComponent]
